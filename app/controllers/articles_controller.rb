cf2c6246029cd222c9ec0de7c5f661557a056460
Serhii T <tocarchuk@gmail.com>
2021-12-09 12:41:30 +0200

    Add CRUD actions

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,57 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+  
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash.now[:success] = "Article successfully created"
+      redirect_to @article
+    else
+      flash.now[:error] = "Something went wrong"
+      render :new
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+      flash.now[:success] = "Article successfully created"
+    else
+      flash.now[:error] = "Something went wrong"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy      
+    flash.now[:success] = 'Object was successfully deleted.'
+    
+    redirect_to root_path
+  end
+  
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
+  
+end
